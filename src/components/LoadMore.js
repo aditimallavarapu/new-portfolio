9c287f99d7eee7ba1e4768d06c8b7c78f0b6382a
Aditi Mallavarapu <m.aditi89@gmail.com>
2020-07-21 23:46:01 -0500

    checking

--- src/components/LoadMore.js
+++ src/components/LoadMore.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 class LoadMore extends React.Component {
     render(){
+        console.log(window.location.pathname);
         return <a href="/aditi-portfolio/all-projects" className="load-more"><i className="fa fa-chevron-down"> Load More Projects</i></a>;
         }
 }
