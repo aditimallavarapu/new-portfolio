18a03e7c6fa1381f71f668921f3a26c62dd48ec1
Aditi Mallavarapu <m.aditi89@gmail.com>
2020-07-22 01:27:34 -0500

    link

--- src/components/LoadMore.js
+++ src/components/LoadMore.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
 class LoadMore extends React.Component {
     render(){
         return <Link to="/aditi-portfolio/projects"  className="load-more"><i className="fa fa-chevron-down"> Load More Projects</i></Link>;
